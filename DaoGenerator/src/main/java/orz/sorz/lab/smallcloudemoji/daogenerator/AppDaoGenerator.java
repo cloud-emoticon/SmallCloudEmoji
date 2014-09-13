7cefb6b670a8c3921755f3dcbb48a899ec83f4de
sorz <orz@sorz.org>
2014-09-14 02:27:08 +0800

    Add entities

--- DaoGenerator/src/main/java/orz/sorz/lab/smallcloudemoji/daogenerator/AppDaoGenerator.java
+++ DaoGenerator/src/main/java/orz/sorz/lab/smallcloudemoji/daogenerator/AppDaoGenerator.java
@@ -2,7 +2,9 @@ package orz.sorz.lab.smallcloudemoji.daogenerator;
 
 import de.greenrobot.daogenerator.DaoGenerator;
 import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
 import de.greenrobot.daogenerator.Schema;
+import de.greenrobot.daogenerator.ToMany;
 
 public class AppDaoGenerator {
 
@@ -17,9 +19,28 @@ public class AppDaoGenerator {
 
     private static void addRepository(Schema schema) {
         Entity repository = schema.addEntity("Repository");
-        repository.addIdProperty();
-        repository.addStringProperty("url").notNull();
+        Property repositoryId = repository.addIdProperty().getProperty();
+        repository.addStringProperty("url").notNull().unique();
         repository.addStringProperty("alias");
         repository.addDateProperty("lastUpdateDate");
+
+        Entity category = schema.addEntity("Category");
+        Property categoryId = category.addIdProperty().getProperty();
+        category.addStringProperty("name");
+        category.addToOne(repository, repositoryId);
+
+        ToMany repositoryToCategories = repository.addToMany(category, repositoryId);
+        repositoryToCategories.setName("categories");
+
+        Entity entry = schema.addEntity("Entry");
+        entry.addIdProperty();
+        entry.addStringProperty("emoticon").notNull();
+        entry.addStringProperty("description");
+        entry.addDateProperty("lastUsed");
+        entry.addToOne(category, categoryId);
+
+        ToMany categoryToEntries = category.addToMany(entry, categoryId);
+        categoryToEntries.setName("entries");
     }
+
 }
