b8c8bc641f73f8d6d3c38e542cc0d46f91ae1b37
sorz <orz@sorz.org>
2014-09-25 01:58:22 +0800

    Implement adding repository basically

---
+++ app/src/main/java/org/sorz/lab/smallcloudemoji/AddRepositoryActivity.java
@@ -0,0 +1,86 @@
+package org.sorz.lab.smallcloudemoji;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.sorz.lab.smallcloudemoji.db.DaoSession;
+import org.sorz.lab.smallcloudemoji.db.DatabaseHelper;
+import org.sorz.lab.smallcloudemoji.db.Repository;
+import org.sorz.lab.smallcloudemoji.db.RepositoryDao;
+
+
+public class AddRepositoryActivity extends Activity {
+    public final static int RESULT_SUCCESS_ADDED = 2;
+    private TextView urlTextView;
+    private TextView aliasTextView;
+    private DaoSession daoSession;
+    private RepositoryDao repositoryDao;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_repository);
+        urlTextView = (TextView) findViewById(R.id.repository_url);
+        aliasTextView = (TextView) findViewById(R.id.repository_alias);
+        daoSession = DatabaseHelper.getInstance(this, true).getDaoSession();
+        repositoryDao = daoSession.getRepositoryDao();
+
+        Uri uri = getIntent().getData();
+        if (uri != null) {
+            if (uri.getScheme().equalsIgnoreCase("cloudemoticon"))
+                urlTextView.setText("http" + uri.toString().substring(13));
+            else
+                urlTextView.setText("https" + uri.toString().substring(14));
+            aliasTextView.requestFocus();
+        }
+    }
+
+    public void cancel(View v) {
+        finish();
+    }
+
+    public void confirm(View v) {
+        final String url = urlTextView.getText().toString().trim();
+        final String alias = aliasTextView.getText().toString().trim();
+
+        if (url.isEmpty() || alias.isEmpty()) {
+            if (url.isEmpty())
+                urlTextView.setError(getString(R.string.input_cannot_be_empty));
+            if (alias.isEmpty())
+                aliasTextView.setError(getString(R.string.input_cannot_be_empty));
+            return;
+        }
+
+        boolean exist = repositoryDao.queryBuilder()
+                .where(RepositoryDao.Properties.Url.eq(url))
+                .count() != 0;
+        if (exist) {
+            urlTextView.setError(getString(R.string.input_repository_exist));
+            return;
+        }
+        Repository repository = new Repository(null, url, alias, false, 0, null);
+
+        new DownloadXmlAsyncTask(this, daoSession) {
+            @Override
+            protected void onPostExecute(Integer result) {
+                super.onPostExecute(result);
+                if (R.string.download_success == result) {
+                    setResult(RESULT_SUCCESS_ADDED);
+                    finish();
+                }
+            }
+        }.execute(repository);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        DatabaseHelper.getInstance(this).close();
+    }
+
+
+}
--- app/src/main/java/org/sorz/lab/smallcloudemoji/RepositoryFragment.java
+++ app/src/main/java/org/sorz/lab/smallcloudemoji/RepositoryFragment.java
@@ -2,10 +2,12 @@ package org.sorz.lab.smallcloudemoji;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
@@ -18,6 +20,7 @@ import org.sorz.lab.smallcloudemoji.db.DatabaseHelper;
  * A fragment representing a list of repositories.
  */
 public class RepositoryFragment extends Fragment {
+    private final static int REQUEST_FOR_ADDING_REPOSITORY = 1;
     private Context context;
     private DaoSession daoSession;
     private RepositoryAdapter adapter;
@@ -48,6 +51,28 @@ public class RepositoryFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.repository_actions, menu);
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_repository_add) {
+            Intent intent = new Intent(context, AddRepositoryActivity.class);
+            startActivityForResult(intent, REQUEST_FOR_ADDING_REPOSITORY);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_FOR_ADDING_REPOSITORY) {
+            if (resultCode == AddRepositoryActivity.RESULT_SUCCESS_ADDED) {
+                // TODO: Refresh repositories list
+            }
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
